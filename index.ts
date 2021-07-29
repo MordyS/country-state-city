ffe5ab7f3580dd32f0d1cdc24a803897f16287f2
MordyFCS <mordy@firstchoiceship.com>
2021-07-29 16:58:04 -0400

    export interfaces

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import Country from './lib/country';
 import State from './lib/state';
+import { ICountry, IState } from './lib/interface';
 
 export { Country };
 export { State };
+export { ICountry, IState };
